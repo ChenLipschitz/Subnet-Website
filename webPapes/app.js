9fd37ceb394d871595354bc5352aa58378ebbf09
chen <root@DESKTOP-PUNVNS3.localdomain>
2024-01-25 10:06:21 +0200

    update react

---
+++ webPapes/app.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>SubNet - Find Your Own Match</h1>
+//       <div className="login-box">
+//         <input type="text" placeholder="Login as a householder" />
+//         <input type="text" placeholder="Login as a subtenant" />
+//         <button>Sign In</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
